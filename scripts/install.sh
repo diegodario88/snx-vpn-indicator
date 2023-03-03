4a1c0ae844cd1154afd1b91b95b456123a1439ea
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-03-03 11:59:51 -0300

    chore: update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 echo "Packing the extension..."
-rm ../snx-vpn-indicator@diegodario88.github.io.shell-extension.zip -v
-gnome-extensions pack ../snx-vpn-indicator@diegodario88.github.io \
-    --out-dir="../"
+
+rm -v snx-vpn-indicator@diegodario88.github.io.shell-extension.zip
+
+gnome-extensions pack snx-vpn-indicator@diegodario88.github.io
 
 echo "Uninstalling old extension..."
 gnome-extensions uninstall snx-vpn-indicator@diegodario88.github.io
 rm -rfv ~/.local/share/gnome-shell/extensions/snx-vpn-indicator@diegodario88.github.io
 
 echo "Installing the extension..."
-gnome-extensions install ../snx-vpn-indicator@diegodario88.github.io.shell-extension.zip
-
-echo "Cleaning up..."
-#rm ../snx-vpn-indicator@diegodario88.github.io.shell-extension.zip
+gnome-extensions install snx-vpn-indicator@diegodario88.github.io.shell-extension.zip
 
 echo "Done! Now restart your session."
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp ../snx-vpn-indicator@diegodario88.github.io/*.js ~/.local/share/gnome-shell/extensions/snx-vpn-indicator@diegodario88.github.io/ -v
+cp snx-vpn-indicator@diegodario88.github.io/*.js ~/.local/share/gnome-shell/extensions/snx-vpn-indicator@diegodario88.github.io/ -v
